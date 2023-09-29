b749a15e55014ff85ec7a324e1473a3e674f6508
Somogyvári Benedek <s.benedek2003@gmail.com>
2023-09-29 12:08:37 +0200

    Update Header
    In the current state it is responsive but still missing some parts

--- src/Header.svelte
+++ src/Header.svelte
@@ -1,8 +1,12 @@
 <script>
+  const currentDate = new Date()
+  const experienceYear = currentDate.getFullYear() - 2021
 
+  let experience = experienceYear < 10 ? '0' + experienceYear : experienceYear
+  let age = currentDate.getMonth() < 10 ? currentDate.getFullYear() - 2004 : currentDate.getFullYear() - 2003
 </script>
 
-<header class="h-screen relative overflow-hidden">
+<header class="flex flex-col h-screen relative overflow-hidden">
   <div class="flex flex-row justify-between px-7 py-3 md:px-12 md:py-6 xl:px-16 xl:py-8">
     <h1 class="font-semibold my-auto xl:text-lg">Somogyvári Benedek</h1>
     <span class="material-symbols-outlined p-2 md:hidden">
@@ -16,19 +20,43 @@
     </ul>
   </div>
 
-  <div class="p-3">
-    <div class="flex items-center">
-      <div class="w-9 bg-primary-200 h-[1px] mx-4"></div>
-      <span class="text-primary-200 text-xs tracking-[3px]">HELLO</span>
+  <div class="flex items-center justify-center w-full flex-1 mb-10">
+    <div class="hello-container p-3">
+      <div class="flex items-center">
+        <div class="w-9 bg-primary-200 h-[1px] mx-4"></div>
+        <span class="text-primary-200 text-xs tracking-[3px]">HELLO</span>
+      </div>
+  
+      <h1 class="font-bold text-4xl my-4">
+        I'm <span class="text-primary-100 tracking-[3px]">BENEDEK</span>
+      </h1>
+  
+      <p class="mt-6 text-lg font-medium">
+        Passionate Developer with a Love for Learning, Exploring New Technologies, and Crafting Digital Experiences
+      </p>
+  
+      <div class="year-cards">
+        <div>
+          <p>{experience}</p>
+          <p>years of <br> experience</p>
+        </div>
+        <div>
+          <p>{age}</p>
+          <p>years old</p>
+        </div>
+      </div>
+  
+      <div class="more-buttons">
+        <button class="bg-gradient-to-r from-primary-200 to-primary-300">
+          Contact me
+          <span class="material-symbols-outlined">chevron_right</span>
+        </button>
+        <button class="border-2 border-zinc-800">
+          About me
+          <span class="material-symbols-outlined">chevron_right</span>
+        </button>
+      </div>
     </div>
-
-    <h1 class="font-bold text-4xl my-3">
-      I'm <span class="text-primary-100 tracking-[3px]">BENEDEK</span>
-    </h1>
-
-    <p class="mt-5 text-lg font-medium">
-      Passionate Developer with a Love for Learning, Exploring New Technologies, and Crafting Digital Experiences
-    </p>
   </div>
 </header>
 
@@ -60,6 +88,43 @@ header::before {
 ul {
   li {
     @apply ml-6 text-sm xl:text-base font-light tracking-[1px];
+  }
+}
+
+.hello-container {
+  @media screen and (min-width: 367px) {
+    width: 367px;
+  }
+}
+
+.year-cards {
+  @apply flex justify-between my-24;
+
+  div {
+  @apply flex items-center;
+
+    p {
+      &:nth-child(1) {
+        @apply text-5xl font-normal mr-3;
+      }
+      
+      &:nth-child(2) {
+        color: #B3B3B3;
+        @apply text-center text-sm;
+      }
+    }
+  }
+}
+
+.more-buttons {
+  @apply flex justify-between;
+
+  button {
+    @apply flex w-[164px] items-center justify-between text-base font-medium py-3 pl-5 pr-3 rounded-lg;
+
+    span {
+      margin-left: 0.75rem;
+    }
   }
 }
 </style>
