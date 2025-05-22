6b87f6b9fec5f133c9f84eb9bf96596aafb59d07
Resongeo <s.benedek2003@gmail.com>
2025-05-22 15:54:51 +0200

    Add gradient above footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,7 +1,7 @@
 <script>
 </script>
 
-<section id="footer" class="w-full bg-black bg-opacity-10 px-3 py-6 lg:py-10">
+<section id="footer" class="w-full bg-black bg-[#131515] px-3 py-6 lg:py-10">
   <div class="flex flex-row items-start justify-around lg:w-[1024px] lg:m-auto">
     <div class="w-48">
       <h1 class="font-light text-neutral-500 text-xl mb-3">
@@ -31,6 +31,22 @@
 </section>
 
 <style lang="scss" scoped>
+  #footer {
+    position: relative;
+  }
+
+  #footer::after{
+    content: '';
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 200px;
+    background-image: linear-gradient(to bottom, transparent, #131515);
+    transform: translateY(-100%);
+    z-index: -1;
+  }
+
   ul {
     li {
       @apply hover:text-primary transition-colors;
