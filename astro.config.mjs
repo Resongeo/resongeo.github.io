9319623e389c8aec67eace305b4f64cc6ccf6a5f
Resongeo <s.benedek2003@gmail.com>
2025-09-02 17:35:13 +0200

    chore: create fesh astro project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import svelte from "@astrojs/svelte";
-import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [svelte(), tailwind()],
-  site: 'https://resongeo.github.io'
-});
+export default defineConfig({});
--- src/lib/Title.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  export let text = ''
-</script>
-
-<h1
-  class="transition-float-up text-center text-4xl md:text-5xl xl:text-6xl font-bold tracking-widest text-primary mb-20 lg:mb-32 xl:mb-40"
->
-  {text}
-</h1>
